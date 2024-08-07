42196624a123fd0c5bf4129d7b08b2b792d01507
SaifySheikh <sheikhmm_4@rknec.edu>
2024-08-07 17:37:47 +0530

    Console added

---
+++ Chapter 1 - Basics/Console/app.js
@@ -0,0 +1,10 @@
+//console is an object which has many methods
+
+// 1. log
+console.log(`SSK Developer`)
+
+// 2. Warning
+console.warn('This is a warning')
+
+//3. Error
+console.error(`This is an Error`)
